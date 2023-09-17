473248c931bf14c0034e51ba39f8668792d9535d
Br-Encina <encinabruno10@gmail.com>
2023-09-17 16:52:17 -0300

    combie los colores del fondo del body, agregue mis simuladores, agrande un poco el tamaño de los input de los formularios

--- src/components/homeBankingWindows/Convertidor.jsx
+++ src/components/homeBankingWindows/Convertidor.jsx
@@ -1,30 +1,106 @@
 import { useEffect, useState } from "react";
 
-function Convertidor() {
-	const [rates, setRates] = useState(0);
-	const [userInput, setUserInput] = useState(1);
+// function Convertidor() {
+// 	const [rates, setRates] = useState(0);
+// 	const [userInput, setUserInput] = useState(1);
 
-	useEffect(() => {
-		fetch("https://open.er-api.com/v6/latest/USD")
-			.then((res) => res.json())
-			.then((res) => setRates(res["rates"]));
-	}, []);
-	return (
-		<div className="cotizacionesPage">
-			<h1>Conversion</h1>
-			<input
-				type="number"
-				placeholder="Monto en AR$"
-				onChange={(e) => setUserInput(e.target.value)}
-				className="conversionInput"
-			/>
-			<h3>En dolares: {(userInput / rates["ARS"]).toFixed(2)}</h3>
-			<h3>
-				En Euros:{" "}
-				{((userInput / rates["ARS"]) * rates["EUR"]).toFixed(2)}
-			</h3>
-		</div>
-	);
+// 	useEffect(() => {
+// 		fetch("https://open.er-api.com/v6/latest/USD")
+// 			.then((res) => res.json())
+// 			.then((res) => setRates(res["rates"]));
+// 	}, []);
+// 	return (
+// 		<div className="cotizacionesPage">
+// 			<h1>Conversion</h1>
+// 			<input
+// 				type="number"
+// 				placeholder="Monto en AR$"
+// 				onChange={(e) => setUserInput(e.target.value)}
+// 				className="conversionInput"
+// 			/>
+// 			<h3>En dolares: {(userInput / rates["ARS"]).toFixed(2)}</h3>
+// 			<h3>
+// 				En Euros:{" "}
+// 				{((userInput / rates["ARS"]) * rates["EUR"]).toFixed(2)}
+// 			</h3>
+// 		</div>
+// 	);
+// }
+
+// export default Convertidor;
+
+function Conversion() {
+
+    // const navigate = useNavigate();
+
+
+
+  const convertir = function(e) {
+    e.preventDefault()
+    const monto = document.getElementById('monto');
+    const moneda = document.getElementById('moneda');
+    const tabla = document.querySelector('tbody')
+
+
+
+    while(tabla.firstChild){
+          tabla.removeChild(tabla.firstChild);
+    }
+    
+   
+    const montoValue = parseFloat(monto.value);
+    const monedaValue = moneda.value;
+
+    let resultado = 0;
+
+    if (monedaValue === 'Dolar') {
+      resultado = montoValue / 735;
+    }
+    if (monedaValue === 'Euro') {
+      resultado = montoValue / 848;
+    }
+    if (monedaValue === 'Real') {
+      resultado = montoValue / 71;
+    }
+ const conversion = document.createElement('tr')
+  conversion.innerHTML = `
+    <td><h2>${montoValue}(Pesos Argetinos)</h2></td>
+    <td><h2>=</h2></td>
+    <td><h2>${resultado.toFixed(2)}(${moneda.value}) </h2></td>
+   `;
+     tabla.appendChild(conversion)
+}
+    return ( <>
+
+<div className="cotizacionesPage">
+	
+<h1> Calculadora de divisas</h1>
+    <form  onSubmit={convertir}>
+
+       <label htmlFor="monto">Introducir monto en pesos:</label>
+       <input className="conversionInput" type="text" id="monto"/>
+	   <br/>
+       <label htmlFor="moneda">Seleccionar moneda</label>
+       <select name="moneda" id="moneda">
+          <option value="Dolar">Dolar</option>
+          <option value="Euro">Euro</option>
+         <option value="Real">Real</option>
+        </select>
+		<br />
+        <button type="submit" id="botonConvertir">Convertir</button>
+    </form>
+
+    
+
+    <table>
+    <tbody></tbody>
+    </table>
+
+
+    <br />
+</div>
+     
+    </> );
 }
 
-export default Convertidor;
+export default Conversion;
--- src/components/homeBankingWindows/Prestamos.jsx
+++ src/components/homeBankingWindows/Prestamos.jsx
@@ -1,30 +1,130 @@
-function Prestamos({ user, setUser }) {
-	function handleSubmit(e) {
-		e.preventDefault();
-		const { monto, plazo } = e.target;
-		if (
-			confirm(
-				`Solicitar un prestamo de $${monto.value} a ${plazo.value} meses?`
-			)
-		) {
-			user.balance += Number(monto.value);
-			window.localStorage.removeItem(user.email);
-			window.localStorage.setItem(user.email, JSON.stringify(user));
-			monto.value = "";
-			plazo.value = "";
-			alert("Prestamo concedido");
-		}
-	}
-	return (
-		<>
-			<h1 id="prestamoH1">Solicitar prestamo</h1>
-			<form onSubmit={(e) => handleSubmit(e)} className="prestamoForm">
-				<input type="number" placeholder="Monto (AR$)" name="monto" />
-				<input type="number" placeholder="Plazo (meses)" name="plazo" />
-				<button>Solicitar</button>
-			</form>
-		</>
-	);
+
+import React, { useRef } from "react";
+
+function Prestamos() {
+    
+    // const navigate = useNavigate();
+    const montoRef = useRef(null);
+    const mesesRef = useRef(null);
+    const interesesRef = useRef(null);
+    const tbodyRef = useRef(null);
+    const tfootRef = useRef(null);
+    const calcularCuota = function (e){
+
+        e.preventDefault()
+
+        let monto = parseFloat(montoRef.current.value);
+        const meses = parseInt(mesesRef.current.value);
+        const intereses = parseFloat(interesesRef.current.value);
+    
+     const completarTabla = document.querySelector('tbody');
+     const Total = document.querySelector('tfoot');
+
+        //reiniciar funcion//
+    
+        while(completarTabla.firstChild){
+            completarTabla.removeChild(completarTabla.firstChild);
+        }
+        while(Total.firstChild){
+            Total.removeChild(Total.firstChild)
+        }
+    
+        
+            //Formato fechas//
+    
+        
+        let fechaActual = new Date();
+        const año = fechaActual.getFullYear();
+        const mes = fechaActual.getMonth() + 1; 
+        const dia = fechaActual.getDate();
+    
+        let pagoIntereses=0, pagoCapital = 0, cuota = 0, totalDeIntereses = 0;
+    
+    
+        //calculo de cuota, con metodo dde amortizacion frances//
+    
+        cuota = monto * (Math.pow(1+intereses/100, meses)*intereses/100)/(Math.pow(1+intereses/100, meses)-1);
+    
+        //lenado de la tabla//
+    
+        if(meses > 72) {
+    
+            alert('El plazo de meses es mas de lo permitido')
+        } else 
+    
+        for(let i = 1; i <= 1; i++) {
+    
+            pagoIntereses = parseFloat(monto*(intereses/100));
+            pagoCapital = cuota - pagoIntereses;
+            monto = parseFloat(monto-pagoCapital);
+            totalDeIntereses = totalDeIntereses + pagoIntereses
+    
+    
+            
+            //llevar los elementos creados a la tabla en el html//
+            
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${dia}/${mes + 1}/${año}</td>
+                <td>${cuota.toFixed(2)}</td>
+                <td>${pagoIntereses.toFixed(2)}</td>
+                
+            `;
+    
+        
+    
+            completarTabla.appendChild(row)
+    
+            
+           //calculo del total a pagar// //en caso de querer que se haga una tabla completa, poner esta parte del cogido fuera del bucle for//
+           const ultimaFila = document.createElement('tr');
+           ultimaFila.innerHTML = `
+           <td>Total a pagar:</td>
+           <td>${(cuota * meses).toFixed(2)}</td>
+            `;
+           Total.appendChild(ultimaFila)
+     
+    }
+    
+    }
+    return ( 
+        <>
+     <h1 id="prestamoH1"> Simulador de Prestamos </h1>
+        <form className="prestamoForm" onSubmit={calcularCuota}>
+
+            <label htmlFor="monto">Monto a solicitar:</label>
+            <input type="text" ref={montoRef} placeholder="Ingresar monto" required/>
+            <br/>
+            <label htmlFor="meses">Tiempo(maximo 72 meses):</label>
+            <input type="text" ref={mesesRef} placeholder="Ingresar tiempo en meses"  required/>
+            <br/>
+            <label htmlFor="intereses">Ingresar el interes mensual del prestamo a solicitar:</label>
+            <input type="text" ref={interesesRef} placeholder="Sin el simbolo de porcentaje" required/>
+            <button type="submit" id="botonCalcular" >Calcular prestamo</button>
+
+        </form>
+
+        
+        <br/>
+            
+        <table>
+             <thead id="tabla">
+                 <tr>
+                   <th>Fecha</th>
+                   <th>Cuota</th>
+                   <th>Interés</th>
+                 </tr>
+             </thead>
+             <tbody ref={tbodyRef}></tbody> 
+             <tfoot ref={tfootRef}></tfoot> 
+
+        </table>
+
+
+
+      
+        </>
+   );
 }
 
 export default Prestamos;
